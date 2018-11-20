d308e4c574de1fb432c798eb8ef7677793bca909
Jayasanka Weerasinghe <jayasanka.sack@gmail.com>
2018-11-20 17:46:18 +0530

    login completed

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,8 +1,8 @@
-var serverAddress = '/myhamper.me'
-var api = serverAddress+'/api/v1'
+var serverAddress = '/projects/agni-targa'
+var api = '/projects/targa-api/v1'
 
 function loadPage(url){
-
+    redirectIfNotLogged();
     $.ajax({
         type: "POST",
         url: 'async/'+url,
@@ -37,5 +37,74 @@ function refreshPage(){
     setTimeout(function () {
         loadPage(pageRosolve(window.location.hash))
     }, 1000);
+}
+
+function login() {
+    var error = $('#lblLoginError');
+    var un = $('#txtLoginEmail').val();
+    var pw = $("#txtLoginPassword").val();
+
+    if (un === "") {
+        error.html("Please Insert Your username"); // check username/ email is empty
+    } else if (pw === "") {
+        error.html('Please Insert Your Password');
+    } else {
+        // send data to API
+        $.ajax({
+            type: "POST",
+            url: api+'/login',
+            data: 'username='+un+'&password=' + pw,
+            dataType: "json",
+
+            success: function (data) { 
+                    error.html('<b style="color:green;">you are logged. wait for redirection</b>');
+                    window.location.replace(serverAddress); //redirect to particullar dashboard page
+                
+            },
+            error: function () {
+                error.html('Wrong Login Data<br/>'); // view error message
+            }
+        });
+    }
+}
+
+function logout() {
+    $.ajax({
+        type: "GET",
+        url: api+'/logout',
+        dataType: "json",
+        success: function (data) {
+            window.location.replace(serverAddress+'/login.html'); //redirect to particullar dashboard page
+
+        },
+        error: function () {
+            error.html('Wrong Login Data<br/>'); // view error message
+        }
+    });
+}
+
+function redirectIfLogged() {
+    console.log("hi");
+    $.ajax({
+        type: "GET",
+        url: api+'/user-status',
+        dataType: "json",
+        success: function (data, textStatus, jqXHR) {
+            console.log(jqXHR.status);
+            window.location.replace(serverAddress);
+        }
+    });
+}
+
+function redirectIfNotLogged() {
+    var logged = false;
+    $.ajax({
+        type: "GET",
+        url: api+'/user-status',
+        dataType: "json",
+        error: function () {
+            window.location.replace(serverAddress+'/login.html');
+        }
+    });
 }
 
